70594787e4e4240cbfa7cd6c333ef4e4294ca6f5
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-10 13:08:03 +0600

    basic mongo setup

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.klfob8q.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
     res.send('Dr. Watson server is running');
